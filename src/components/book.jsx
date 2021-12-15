f12df1e01063361f3f36d67c0098c6e12c283843
Adarsh999bh <adarshbhandary48@gmail.com>
2021-12-15 20:48:51 +0530

    added display book code with sortby price and search book functionality

---
+++ src/components/book.jsx
@@ -0,0 +1,120 @@
+import {
+    Grid,
+    Box,
+    Typography,
+    Button,
+    Menu,
+    MenuItem,
+    Pagination,
+  } from "@mui/material";
+  import React from "react";
+  import { useSelector } from "react-redux";
+  import { useDispatch } from "react-redux";
+  import productService from "../service/productService";
+  import { setBooks } from "../actions/productActions";
+  import BookCard from "./bookcard";
+  import styled from "@emotion/styled";
+  
+  const Paginations = styled(Pagination)(({ theme }) => ({
+    color: "#A03037",
+  }));
+  
+  const Book = () => {
+    const myBooks = useSelector((state) => state.products.filteredProducts);
+
+    const numberOfBooks = myBooks.length;
+  
+    const dispatch = useDispatch();
+  
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
+  
+    const handlePagination = (index) => {
+    productService
+        .getBooks(index)
+        .then((res) => {
+          console.log(res.data);
+          dispatch(setBooks(res.data));
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    };
+  
+    const handleDisplayOrder = (order) => {
+      if (order === "low") {
+        myBooks.sort((a, b) => a.price - b.price);
+        handleClose();
+      } else {
+        myBooks.sort((a, b) => b.price - a.price);
+        handleClose();
+      }
+    };
+  
+    return (
+      <Box className="main-container">
+        <Grid container>
+          <Grid item xs={6} align="left">
+            <Typography id="book-count">
+              Books
+              <span id="book-count-span">({numberOfBooks} items)</span>
+            </Typography>
+          </Grid>
+          <Grid item xs={6} align="right">
+            <Button
+              id="sort-by-btn"
+              aria-controls="basic-menu"
+              aria-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
+              onClick={handleClick}
+            >
+              Sort by price
+            </Button>
+            <Menu
+              id="basic-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              MenuListProps={{
+                "aria-labelledby": "basic-button",
+              }}
+            >
+              <MenuItem onClick={() => handleDisplayOrder("low")}>
+                price: low to high
+              </MenuItem>
+              <MenuItem onClick={() => handleDisplayOrder("high")}>
+                price: high to low
+              </MenuItem>
+            </Menu>
+          </Grid>
+        </Grid>
+  
+        <Grid container spacing={4}>
+          {myBooks.map((item, index) => {
+            return <BookCard item={item} key={index} />;
+          })}
+        </Grid>
+  
+        <div id="pagination">
+          <Paginations
+            count={5}
+            shape="rounded"
+            color="primary"
+            onChange={(event, page) => handlePagination(page)}
+          />
+        </div>
+      </Box>
+    );
+  };
+  
+  export default Book;
---
+++ src/components/bookcard.jsx
@@ -0,0 +1,101 @@
+import React, { useState } from "react";
+import {
+  CardContent,
+  Card,
+  Typography,
+  CardActions,
+  Button,
+  Grid,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+} from "@mui/material";
+import cartService from "../service/cartService";
+const BookCard = ({ item }) => {
+  const [wishlist, setWishlist] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [cart, setCart] = useState(false);
+  const handleCart = () => {
+    let data = {
+      productId: item._id,
+      quantity: 1,
+      cartStatus:"open",
+      isWishList:false
+    };
+    setWishlist(true);
+    cartService
+      .addToCart(data)
+      .then((res) => {
+          console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const handleWishlist = () => {
+    setCart(true);
+  };
+
+  return (
+    <Grid item xs={12} sm={6} md={3}>
+      <Card sx={{ height: 345 }} onClick={() => setIsOpen(true)}>
+        <div className="imageContainer">
+          <img className="bookImage" src={item.image} alt="" />
+        </div>
+        <CardContent>
+          <Typography align="left" className="item-content">
+            {item.productTitle}
+          </Typography>
+          <Typography
+            align="left"
+            color="text.secondary"
+            style={{ fontSize: "14px" }}
+          >
+            by {item.author}
+          </Typography>
+          <Typography
+            align="left"
+            style={{ fontWeight: "bold", fontSize: "14px" }}
+          >
+            Rs. {item.price}
+          </Typography>
+        </CardContent>
+        <CardActions
+          style={{ display: "flex", justifyContent: "space-around" }}
+        >
+          <Button
+            onClick={handleCart}
+            fullWidth="true"
+            style={
+              !cart && wishlist
+                ? { background: "#3371B5", color: "white" }
+                : !cart
+                ? { backgroundColor: "#A03037", color: "white" }
+                : { display: "none" }
+            }
+          >
+            {!cart && wishlist ? "Added to bag" : "Add to bag"}
+          </Button>
+          <Button
+            style={
+              !wishlist
+                ? { border: "1px solid black", color: "black" }
+                : { display: "none" }
+            }
+            fullWidth="true"
+            onClick={handleWishlist}
+          >
+            Wishlist
+          </Button>
+        </CardActions>
+      </Card>
+      <Dialog open={isOpen} onClose={() => setIsOpen(false)}>
+        <DialogTitle>{item.title}</DialogTitle>
+        <DialogContent>{item.description}</DialogContent>
+      </Dialog>
+    </Grid>
+  );
+};
+
+export default BookCard;
